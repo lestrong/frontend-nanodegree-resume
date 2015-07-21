903c9a1903b7e43e0d196ee4b1207abff5c28765
LoRae Strong <lstrong@chi.yearup.org>
2015-07-21 15:44:03 -0500

    Header Change 1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,20 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+//Work in progress
+
+//Header
+var name = "LoRae Strong";
+var formattedName = HTMLheaderName.replace("%data%", name);
+$("#header").prepend(formattedName);
+
+var role = "Web Knight";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").append(formattedRole);
+
+/*var email = "lstrong@chi.yearup.org";
+var formattedEmail = HTMLemail.replace("%data%", email);
+$("#header").append(formattedEmail); */
+
+
+
+var school = "Year Up (Chicago)";
+var formattedSchool = HTMLschoolName.replace("%data%", school);
+$("#education").append(formattedSchool);
